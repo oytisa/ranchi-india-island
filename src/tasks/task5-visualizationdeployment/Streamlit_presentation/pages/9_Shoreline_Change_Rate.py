ee9a775c5675b956897e55a6b08deb672714ce16
halajadallah <htree2000@yahoo.com>
2022-08-27 08:36:13 -0500

    updated transect classes

--- src/tasks/task5-visualizationdeployment/Streamlit_presentation/pages/9_Shoreline_Change_Rate.py
+++ src/tasks/task5-visualizationdeployment/Streamlit_presentation/pages/9_Shoreline_Change_Rate.py
@@ -36,19 +36,19 @@ gdf = geopandas.read_file(path)
 gdf_o = gdf
 gdf = gdf_o[8:] #the first 8 entries are NANs
 #make classes and label them using cuts by change rate
-gdf['LRR_cut'] = pd.cut(gdf['LRR'], bins = [-53, -35, -20, -10, 0, 10, 20, 30, 40], 
-                                    labels = ['Sever Erosion', 'Large Erosion','Moderate Erosion', 'Low Erosion', 'Low Accretion', 'Medium Accretion', 'Large Accretion', 
-                                    'Extreme Accretion'])
+gdf['LRR_cut'] = pd.cut(gdf['LRR'], bins = [-30, -20, -10, 0, 10, 20, 31], 
+                                    labels = ['Large Erosion','Moderate Erosion', 'Low Erosion', 
+                                    'Low Accretion', 'Medium Accretion', 'Large Accretion']) 
+                                   
                                     
 #Make color labels for                                    
-color_dict = {'Sever Erosion': '#800000', #maroon/brown
+color_dict = {
               'Large Erosion': '#ff0000', #red
               'Moderate Erosion': '#ff9933', #orange
               'Low Erosion': '#ffff00',  #yellow
               'Low Accretion': '#00cc66', #green 
               'Medium Accretion': '#3399ff', #blue 
               'Large Accretion': '#6600cc',  #purple 
-              'Extreme Accretion': '#000000' #black 
               }
                                     
 gdf['LRR_color'] = gdf['LRR_cut'].map(color_dict)
@@ -56,31 +56,28 @@ gdf['LRR_color'] = gdf['LRR_cut'].map(color_dict)
 #examine what is in geojson file
 #gdf.to_file('transects.geojson', driver = 'GeoJSON')
 
-'''
-Layer each group of Transects seperately
-First we create the dataframes 
-'''
+
+#Layer each group of Transects seperately
+#First we create the dataframes 
+
 gdf_erosion_low = gdf[gdf['LRR_cut'] == 'Low Erosion']
 gdf_erosion_medium = gdf[gdf['LRR_cut'] == 'Moderate Erosion']
 gdf_erosion_high = gdf[gdf['LRR_cut'] == 'Large Erosion']
-gdf_erosion_extr = gdf[gdf['LRR_cut'] == 'Sever Erosion']
 
 gdf_accretion_low = gdf[gdf['LRR_cut'] == 'Low Accretion']
 gdf_accretion_medium = gdf[gdf['LRR_cut'] == 'Medium Accretion']
 gdf_accretion_high = gdf[gdf['LRR_cut'] == 'Large Accretion']
-gdf_accretion_extr = gdf[gdf['LRR_cut'] == 'Extreme Accretion']
 
 
 style_fun = lambda x: {'color': x['properties']['LRR_color']} 
 folium.GeoJson(data = gdf_erosion_low[['geometry','LRR_color']], style_function = style_fun, name = 'Low Erosion: -10 < LRR < 0 ').add_to(sagar)
 folium.GeoJson(data = gdf_erosion_medium[['geometry','LRR_color']], style_function = style_fun, name = 'Medium Erosion: -20 < LRR <-10 ').add_to(sagar)
-folium.GeoJson(data = gdf_erosion_high[['geometry','LRR_color']], style_function = style_fun, name = 'High Erosion: -35 < LRR < -20 ').add_to(sagar)
-#folium.GeoJson(data = gdf_erosion_extr[['geometry','LRR_color']], style_function = style_fun, name = 'Extreme Erosion: -53 < LRR < -35 ').add_to(sagar)
+folium.GeoJson(data = gdf_erosion_high[['geometry','LRR_color']], style_function = style_fun, name = 'High Erosion: -30 < LRR < -20 ').add_to(sagar)
+
 
 folium.GeoJson(data = gdf_accretion_low[['geometry','LRR_color']], style_function = style_fun, name = 'Low Accretion: 0 < LRR < 10 ').add_to(sagar)#
 folium.GeoJson(data = gdf_accretion_medium[['geometry','LRR_color']], style_function = style_fun, name = 'Medium Accretion: 10 < LRR < 20 ').add_to(sagar)
-folium.GeoJson(data = gdf_accretion_high[['geometry','LRR_color']], style_function = style_fun, name = 'High Accretion: 20 < LRR < 30 ').add_to(sagar)
-#folium.GeoJson(data = gdf_accretion_extr[['geometry','LRR_color']], style_function = style_fun, name = 'Exteme Accretion: 30 < LRR < 40 ').add_to(sagar)
+folium.GeoJson(data = gdf_accretion_high[['geometry','LRR_color']], style_function = style_fun, name = 'High Accretion: 20 < LRR < 31 ').add_to(sagar)
 
 
 #predicted shorelines of 2030 and 2040
