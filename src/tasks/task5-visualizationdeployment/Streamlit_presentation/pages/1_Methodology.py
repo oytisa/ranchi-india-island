d559f203e6e41bcea45faa8a54d1e73dda751397
halajadallah <htree2000@yahoo.com>
2022-08-29 10:16:13 -0500

    recent update

--- src/tasks/task5-visualizationdeployment/Streamlit_presentation/pages/1_Methodology.py
+++ src/tasks/task5-visualizationdeployment/Streamlit_presentation/pages/1_Methodology.py
@@ -1,18 +1,31 @@
 import streamlit as st
 
 st.header('Methodology')
-st.markdown(
-'''
-* Process Google Earth Engine (GEE) satellite images of Sagar Island to get water areas for the years 1990, 2000, 2010 and 2020.
-* Test few methods for automated shoreline detection
-    * Method 1: GEE Otsu’s thresholding algorithm
-    * Method 2: CoastSat module 
-* GEE generate shapefile of shorelines for the four years
-* Using ArcGIS and DSAS: 
-    * Digitize shoreline more accurately to get accurate transects at select shoreline locations
-    * Generat Transects along the digitized shoreline.
-    * Compute end poit rate (EPR) linear regression rates (LRR) of change (erosion or accretion).
-    * Predict future change in shoreline.  
 
-'''
+st.markdown(
+    """
+    <style>
+    .small-font {
+        font-size:12px;
+        font-style: italic;
+        color: #b1a7a6;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.markdown(
+    '''
+    * Process Google Earth Engine (GEE) satellite images of Sagar Island to get water areas for the years 1990, 2000, 2010 and 2020.
+    * Test few methods for automated shoreline detection
+        * Method 1: GEE Otsu’s thresholding algorithm
+        * Method 2: CoastSat module 
+    * GEE generate shapefile of shorelines for the four years
+    * Using ArcGIS and DSAS: 
+        * Digitize shoreline more accurately to get accurate transects at select shoreline locations
+        * Generat Transects along the digitized shoreline.
+        * Compute end poit rate (EPR) linear regression rates (LRR) of change (erosion or accretion).
+        * Predict future change in shoreline.  
+    '''  
 )
