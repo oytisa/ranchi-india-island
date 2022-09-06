d48377bbc9d4dd9d3cf8183ee21df4a6e13c7702
halajadallah <37782935+halajadallah@users.noreply.github.com>
2022-09-06 07:23:09 -0500

    added description of CoastSat experience

--- src/tasks/task5-visualizationdeployment/Streamlit_presentation/pages/5_CoastSat.py
+++ src/tasks/task5-visualizationdeployment/Streamlit_presentation/pages/5_CoastSat.py
@@ -6,6 +6,9 @@ st.write("Here we see the output of CoastSat shoreline detection.")
 st.markdown(
 '''
 The library applies image classification and sub-pixel resolution to MNDWI images along with OTSU's thresholding algorithm.
+We found some difficulties. Firstly, we did not realise the package outputs a geojson file of multipoints of the shorelines it detects. 
+Secondly, after the end of the project we came back it find the geojson file requires some processing for further use. In particulr, the points 
+along the shoreline are quite dense.
 '''
 )
 st.image("data/coastsat_output.PNG") 
