eb3c4d61058abf2a0c0af8f8fb31082dd24ff398
halajadallah <37782935+halajadallah@users.noreply.github.com>
2022-09-06 08:26:46 -0500

    updated Data Collection page.

--- src/tasks/task5-visualizationdeployment/Streamlit_presentation/pages/2_Data_Collection.py
+++ src/tasks/task5-visualizationdeployment/Streamlit_presentation/pages/2_Data_Collection.py
@@ -13,7 +13,7 @@ df = pd.read_csv(file_path)
 st.header('Data Collection')
 st.markdown(
 '''
-Landasat  Level 2 Surface Reflectence Data was collected using Google Earth Engine
+Landasat  Level 2 Surface Reflectence Data was collected using Google Earth Engine with cloud cover less than 10%. This is most likely in the months of November and December. 
 
 Below is a list of image IDs
 '''
